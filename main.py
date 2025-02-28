8a533fe006eabbc97b9dd53750504413d9448893
somnuk2 <somnuk@mju.ac.th>
2025-02-28 13:42:40 +0700

    Deploy FastAPI to Vercel

--- main.py
+++ main.py
@@ -55,3 +55,6 @@ async def predict_image(file: UploadFile = File(...)):  # Ensure the file is req
     
     except Exception as e:
         return {"error": str(e)}
+    
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
