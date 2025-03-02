02f4761592155a23cff6be3b8abc0b2a70381305
somnuk2 <somnuk@mju.ac.th>
2025-03-02 23:42:13 +0700

    deploy with rendoer

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 import uvicorn
 import pandas as pd
+import os
 
 # Model parameters
 img_height = 224
@@ -57,4 +58,4 @@ async def predict_image(file: UploadFile = File(...)):  # Ensure the file is req
         return {"error": str(e)}
     
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0",  port=int(os.getenv("PORT", 8000)), reload=True)
